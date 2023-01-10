67c121b9a7d6f62e1cf533b8e33ef0b8b09d9d85
Pc <kevsertemur@gmail.com>
2023-01-10 22:35:36 +0300

    cucumber

--- src/test/java/stepdefinitions/GoogleStepDefinitions.java
+++ src/test/java/stepdefinitions/GoogleStepDefinitions.java
@@ -1,5 +1,6 @@
 package stepdefinitions;
 
+
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
@@ -8,25 +9,59 @@ import pages.GoogleSearchPage;
 import utilities.Driver;
 
 public class GoogleStepDefinitions {
-
-
-    @Given("kullanıcı google sayfasındadır")
-    public void kullanıcıGoogleSayfasındadır() {
+    @Given("Kullanici google sayfasindadir")
+    public void kullaniciGoogleSayfasindadir() {
 
         Driver.getDriver().get("https://www.google.com/");
     }
 
-    @When("kullanıcı samsung kelimesini arar")
-    public void kullanıcıSamsungKelimesiniArar() {
+    @When("Kullanici samsung kelimesini arar")
+    public void kullaniciSamsungKelimesiniArar() {
 
         GoogleSearchPage sp = new GoogleSearchPage(Driver.getDriver());
+
         sp.searchFor("samsung");
+
     }
 
-    @Then("kullanıcı sayfada samsung kelimesi geçtiğini doğrular")
-    public void kullanıcıSayfadaSamsungKelimesiGeçtiğiniDoğrular() {
+    @Then("Kullanici sayfada samsung kelimesi gectigini dogrular")
+    public void kullaniciSayfadaSamsungKelimesiGectiginiDogrular() {
 
         Assert.assertTrue(Driver.getDriver().getTitle().contains("samsung"));
+
+        Driver.closeDriver();
+    }
+
+        
+        
+    @When("Kullanici cucumber kelimesini arar")
+    public void kullaniciCucumberKelimesiniArar() {
+
+        GoogleSearchPage sp = new GoogleSearchPage(Driver.getDriver());
+
+        sp.searchFor("cucumber");
+    }
+
+    @Then("Kullanici sayfada cucumber kelimesi gectigini dogrular")
+    public void kullaniciSayfadaCucumberKelimesiGectiginiDogrular() {
+
+        Assert.assertTrue(Driver.getDriver().getTitle().contains("cucumber"));
+
+        Driver.closeDriver();
+    }
+
+    @When("Kullanici selenium kelimesini arar")
+    public void kullaniciSeleniumKelimesiniArar() {
+
+        GoogleSearchPage sp = new GoogleSearchPage(Driver.getDriver());
+
+        sp.searchFor("selenium");
+    }
+
+    @Then("Kullanici sayfada selenium kelimesi gectigini dogrular")
+    public void kullaniciSayfadaSeleniumKelimesiGectiginiDogrular() {
+
+        Assert.assertTrue(Driver.getDriver().getTitle().contains("selenium"));
 
         Driver.closeDriver();
     }
